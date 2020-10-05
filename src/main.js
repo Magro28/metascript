3596229358907f5fbf1721fd24637a0f7d454d04
Mario Groß <turgeon28@gmail.com>
2020-10-05 17:47:52 +0200

    Add gallery

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import VuePictureSwipe from 'vue-picture-swipe';
+Vue.component('vue-picture-swipe', VuePictureSwipe);
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Art.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="art">
+    <h1>This is an Art page</h1>
+    <vue-picture-swipe :items="items"></vue-picture-swipe>
+  </div>
+</template>
+<script>
+  // eslint-disable-next-line no-unused-vars
+  import VuePictureSwipe from 'vue-picture-swipe';
+  export default {
+    data() {
+      return {
+        items: [{
+          src: 'http://via.placeholder.com/600x400',
+          thumbnail: 'http://via.placeholder.com/64x64',
+          w: 600,
+          h: 400,
+          title: 'Hallo'
+        },
+        {
+          src: 'http://via.placeholder.com/1200x900',
+          thumbnail: 'http://via.placeholder.com/64x64',
+          w: 1200,
+          h: 900,
+          title: 'Hallo'
+        }
+      ]};
+    }
+  }
+</script>
