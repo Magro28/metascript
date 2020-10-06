1c88f834d53635e4127c6e0bb080fd8fe44ba629
Mario Groß <turgeon28@gmail.com>
2020-10-06 15:17:23 +0200

    Add real art pics

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <video-background 
+    src="../assets/vid/Berg_und_Wald.mp4"
+    style="max-height: 600px; height: 100vh;"
+    >
+     <h1>This is an about page</h1>
+  </video-background>
+
   </div>
 </template>
--- src/views/Art.vue
+++ src/views/Art.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="art">
-    <h1>This is an Art page</h1>   
-   <video-background 
-    src="../assets/vid/hd0983.mp4"
-    style="max-height: 400px; height: 100vh;"
-    overlay="linear-gradient(45deg,#2a4ae430,#fb949e6b)"
+  <video-background 
+    src="../assets/vid/savanna.mp4"
+    style="max-height: 600px; height: 100vh;"
  >
-      <h1 style="color: white;">Hello welcome!</h1>
+ <!-- overlay="linear-gradient(45deg,#2a4ae430,#fb949e6b)" -->
+      <h1 style="color: white;">Hello welcome to my little art section!</h1>
       <vue-picture-swipe :items="items"></vue-picture-swipe>
+       <p style="color: white;">Drawing is just one of my other hobbies I do when I'm in the mood.
+         Usually I fiddle around with ProCreate on my Ipad, sometimes I'm crazy and use real tools in the real world like my pencil set. 
+       </p>
+       <p>So here are some of my works. Hove fun!</p>
   </video-background>
   </div>
 </template>
@@ -18,18 +21,41 @@
     data() {
       return {
         items: [{
-          src: 'http://via.placeholder.com/600x400',
-          thumbnail: 'http://via.placeholder.com/64x64',
-          w: 600,
-          h: 400,
-          title: 'Hallo'
+          src: '../assets/art/savanna.png',
+          thumbnail: '../assets/art/savannaThumb.png',
+          w: 1366,
+          h: 1024,
+          title: 'Savanna Sunset by Mario Groß'
         },
         {
-          src: 'http://via.placeholder.com/1200x900',
-          thumbnail: 'http://via.placeholder.com/64x64',
-          w: 1200,
-          h: 900,
-          title: 'Hallo'
+          src: '../assets/art/Berg_und_Wald.png',
+          thumbnail: '../assets/art/Berg_und_WaldThumb.png',
+          w: 1366,
+          h: 1024,
+          title: 'Mountain Forest by Mario Groß'
+        },
+        {
+          src: '../assets/art/Das_Auge.png',
+          thumbnail: '../assets/art/Das_AugeThumb.png',
+          w: 1366,
+          h: 1024,
+          title: 'The Eye by Mario Groß'
+        }
+        ,
+        {
+          src: '../assets/art/Wintertime.png',
+          thumbnail: '../assets/art/WintertimeThumb.png',
+          w: 1366,
+          h: 1024,
+          title: 'Winter landscape ☃️ by Mario Groß'
+        }
+        ,
+        {
+          src: '../assets/art/Bonfire.png',
+          thumbnail: '../assets/art/BonfireThumb.png',
+          w: 1366,
+          h: 1024,
+          title: 'Bonfire in Waterfall Country by Mario Groß'
         }
       ]};
     }
