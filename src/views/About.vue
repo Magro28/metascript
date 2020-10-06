17c9faaa049e43d01180ff6af3f05c8caf8b5463
Mario Groß <turgeon28@gmail.com>
2020-10-06 19:22:30 +0200

    Add some texts

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,6 +5,19 @@
     style="max-height: 600px; height: 100vh;"
     >
      <h1>This is an about page</h1>
+     <p>
+      The website is made with vue.js, some npm modules for the background videos and the gallery.
+      Contentwise it's my own artworks.
+    </p>
+    <h2> Todos for now:</h2>
+    
+      <p>Better home page with some examples what I do</p>
+      <p>Some data science showcase</p>
+      <p>Dev/Ops Kubernetes Things</p>
+      <p>Cooler menus</p>
+      <p>Things about my travels</p>
+      <p>More paintings</p>
+    
   </video-background>
 
   </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/me2.jpg">
+    <h1> Welcome to Metascript </h1>
+    <p>
+      At the moment this page is just a place where I experiement with some web frameworks and try out new stuff.
+      I've a lot of todos and ideas but for now I just want to deploy the site.
+    </p>
+    <img alt="Me logo" src="../assets/me2.jpg">
+    <p>Have fun, Mario</p>
    
   </div>
 </template>
